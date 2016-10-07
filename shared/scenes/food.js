538feedbc95239b7cc4604d8c5e3142b5c176bee
Taylor Lodge <taylor@boost.co.nz>
2016-10-07 13:00:14 +1300

    Food screen mostly working
     - Mark/unmark day as fed
     - Calculate which days need feeding
     - Save/restore days Ed was fed on

--- shared/scenes/food.js
+++ shared/scenes/food.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
   Image,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  TouchableHighlight,
+  AsyncStorage
 } from 'react-native';
 import moment from 'moment';
 import _ from 'lodash';
@@ -34,7 +36,7 @@ const styles = {
 };
 
 const foodImage = require('../../images/Food.png');
-const Day = ({name, fedOn, toFeed}) => {
+const Day = ({name, fedOn, toFeed, onClick}) => {
   const viewStyle = {
     marginLeft: 10,
     marginRight: 10,
@@ -43,18 +45,20 @@ const Day = ({name, fedOn, toFeed}) => {
   };
   const textStyle = {
     fontFamily: 'Avenir',
-    color: fedOn ? 'red' : toFeed ? 'green' : '#1FA7FF'
+    color: fedOn ? 'green' : toFeed ? 'red' : '#1FA7FF'
   };
 
   return (
-    <View style={viewStyle}>
-      <Image source={foodImage} style={styles.dayImage} />
-      <Text style={textStyle}>{name}</Text>
-    </View>
+    <TouchableHighlight onPress={onClick}>
+      <View style={viewStyle}>
+        <Image source={foodImage} style={styles.dayImage} onClick={() => console.log('foooo')}/>
+        <Text style={textStyle}>{name}</Text>
+      </View>
+    </TouchableHighlight>
   );
 };
 
-const Food = (props) => {
+const Food = ({markFed, fedOnDays, toFeedOn, days}) => {
   let scrollView;
   const scrollToCenter = (width, height) => {
     // I have no idea, the scrollView ends up being larger than the size of the elements for some reason??
@@ -64,10 +68,13 @@ const Food = (props) => {
   };
 
   const dateOffsets = _.range(-7, 8);
-  const days = _.map(dateOffsets, (offset, index) => {
+  const dayComponents = _.map(dateOffsets, (offset, index) => {
     const date = moment().startOf('day').add(offset, 'days');
+    const day = date.format('dddd');
+    const fedOnThisDay = _.some(fedOnDays, d => d.isSame(date))
+    const toFeedOnThisDay = _.some(toFeedOn, d => d.isSame(date));
 
-    return <Day name={date.format('dddd')} key={index} />;
+    return <Day name={day} key={index} fedOn={fedOnThisDay} toFeed={toFeedOnThisDay} onClick={markFed.bind(null, date)}/>;
   });
 
   return (
@@ -80,7 +87,7 @@ const Food = (props) => {
           onContentSizeChange={scrollToCenter}
           ref={(sv) => { scrollView = sv }}
         >
-          {days}
+          {dayComponents}
         </ScrollView>
       </View>
       <View style={styles.footer}>
@@ -90,4 +97,77 @@ const Food = (props) => {
   );
 };
 
-export default Food;
+class FoodState extends Component {
+  constructor() {
+    super();
+
+    const dateOffsets = _.range(-7, 8);
+    const days = _.map(dateOffsets, (offset) => {
+      const date = moment().startOf('day').add(offset, 'days');
+
+      return date;
+    });
+
+    this.state = {
+      fedOn: [],
+      toFeed: [],
+      days,
+    }
+
+    _.bindAll(this, 'markFed', 'generateToFeedDays');
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('fedOn').then(value => {
+      if(_.isNil(value))
+        return;
+
+      this.setState({
+        fedOn: _.map(
+          JSON.parse(value),
+          date => moment(date)
+        )
+      }, this.generateToFeedDays);
+    });
+  }
+
+  componentWillUpdate() {
+    AsyncStorage.setItem('fedOn', JSON.stringify(this.state.fedOn));
+  }
+
+  markFed(date) {
+    let fedOn;
+
+    if(_.some(this.state.fedOn, d => d.isSame(date)))
+      fedOn = _.filter(this.state.fedOn, d => !d.isSame(date))
+    else {
+      fedOn = [...this.state.fedOn]
+      fedOn.push(date);
+    }
+
+    this.setState({fedOn}, this.generateToFeedDays)
+  }
+
+  generateToFeedDays() {
+    const sortedFeedingDays = this.state.fedOn.sort((a, b) => {
+      return a.isBefore(b) ? -1 : 1;
+    });
+    const lastFedOn = _.last(sortedFeedingDays);
+    const daysToCheck = _.filter(this.state.days, d => d.isAfter(lastFedOn));
+
+    const daysToFeedOn = _.filter(daysToCheck, (d, index) => index % 2 != 0);
+
+    this.setState({toFeed: daysToFeedOn});
+  }
+
+  render() {
+    return <Food
+      markFed={this.markFed}
+      fedOnDays={this.state.fedOn}
+      toFeedOn={this.state.toFeed}
+      days={this.state.days}
+    />;
+  }
+}
+
+export default FoodState;
